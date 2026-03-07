af0d9f7a01d45e44f4800c3e0e1f18f5564138c0
Bam-Manu-182 <bammanuel182@gmail.com>
2026-03-06 20:46:36 -0500

    Project 174 V.1.2.2.2
    Ajustes de archivo para badge CI

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -25,13 +25,13 @@ def generate_diff(file_path1, file_path2):
     for key in keys:
         if key in data1 and key in data2:
             if data1[key] == data2[key]:
-                result += f"  {key}: {stringify(data1[key])}\n"
+                result += f"    {key}: {stringify(data1[key])}\n"
             else:
-                result += f" - {key}: {stringify(data1[key])}\n"
-                result += f" + {key}: {stringify(data2[key])}\n"
+                result += f"  - {key}: {stringify(data1[key])}\n"
+                result += f"  + {key}: {stringify(data2[key])}\n"
         elif key in data1:
-            result += f" - {key}: {stringify(data1[key])}\n"
+            result += f"  - {key}: {stringify(data1[key])}\n"
         else:
-            result += f" + {key}: {stringify(data2[key])}\n"
+            result += f"  + {key}: {stringify(data2[key])}\n"
     result += "}"
     return result
--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -13,12 +13,12 @@ def test_generate_diff():
 
     expected = (
         "{\n"
-        "- follow: false\n"
-        "  host: hexlet.io\n"
-        "- proxy: 123.234.53.22\n"
-        "- timeout: 50\n"
-        "+ timeout: 20\n"
-        "+ verbose: true\n"
+        "  - follow: false\n"
+        "    host: hexlet.io\n"
+        "  - proxy: 123.234.53.22\n"
+        "  - timeout: 50\n"
+        "  + timeout: 20\n"
+        "  + verbose: true\n"
         "}"
     )
 
